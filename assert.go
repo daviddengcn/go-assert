254ac4469f07616bca727bb5c1041454e52daf6f
David Deng <daviddengcn@gmail.com>
2013-12-07 23:03:42 -0800

    ADD caller's position

--- assert.go
+++ assert.go
@@ -2,6 +2,8 @@ package assert
 
 import (
 	"fmt"
+	"path"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -23,13 +25,22 @@ func showText(text string) string {
 	return string(buf)
 }
 
+// Default: skip == 0
+func assertPos(skip int) string {
+	_, file, line, ok := runtime.Caller(skip + 2)
+	if !ok {
+		return ""
+	}
+	return fmt.Sprintf("%s:%d: ", path.Base(file), line)
+}
+
 /*
 	Equals fails the test and shows error message when act and exp are not
 	equal
 */
 func Equals(t *testing.T, name string, act, exp interface{}) {
 	if act != exp {
-		t.Errorf("%s is expected to be %s, but got %v", name,
+		t.Errorf("%s%s is expected to be %s, but got %v", assertPos(0), name,
 			showText(fmt.Sprint(exp)), showText(fmt.Sprint(act)))
 	}
 }
@@ -41,7 +52,7 @@ func Equals(t *testing.T, name string, act, exp interface{}) {
 func StringEquals(t *testing.T, name string, act, exp interface{}) {
 	actS, expS := fmt.Sprint(act), fmt.Sprint(exp)
 	if actS != expS {
-		t.Errorf("%s is expected to be %s, but got %v", name,
+		t.Errorf("%s%s is expected to be %s, but got %v", assertPos(0), name,
 			showText(expS), showText(actS))
 	} // if
 }
@@ -52,20 +63,16 @@ func StringEquals(t *testing.T, name string, act, exp interface{}) {
 */
 func StrSetEquals(t *testing.T, name string, act, exp villa.StrSet) {
 	if !act.Equals(exp) {
-		t.Errorf("%s is expected to be %s, but got %v", name,
+		t.Errorf("%s%s is expected to be %s, but got %v", assertPos(0), name,
 			showText(fmt.Sprint(exp)), showText(fmt.Sprint(act)))
 	}
 }
 
-/*
-	LinesEqual fails the test and shows the error message and line-to-line
-	differences of the lines when two slices of strings are not equal
-*/
-func LinesEqual(t *testing.T, name string, act, exp []string) {
+func linesEquals(t *testing.T, name string, act, exp []string) {
 	if villa.StringSlice(exp).Equals(act) {
 		return
 	}
-	title := fmt.Sprintf("Unexpected %s: ", name)
+	title := fmt.Sprintf("%sUnexpected %s: ", assertPos(1), name)
 	if len(exp) == len(act) {
 		title = fmt.Sprintf("%sboth %d lines", title, len(exp))
 	} else {
@@ -105,6 +112,14 @@ func LinesEqual(t *testing.T, name string, act, exp []string) {
 }
 
 /*
+	LinesEqual fails the test and shows the error message and line-to-line
+	differences of the lines when two slices of strings are not equal
+*/
+func LinesEqual(t *testing.T, name string, act, exp []string) {
+	linesEquals(t, name, act, exp)
+}
+
+/*
 	TextEquals splits input strings into lines and calls LinesEqual
 */
 func TextEquals(t *testing.T, name string, act, exp string) {
@@ -121,7 +136,7 @@ func TextEquals(t *testing.T, name string, act, exp string) {
 */
 func NoError(t *testing.T, err error) bool {
 	if err !=  nil {
-		t.Error(err)
+		t.Errorf("%s%s", assertPos(0), fmt.Sprintf("%v", err))
 		return false
 	}
 	return true
@@ -131,9 +146,9 @@ func NoError(t *testing.T, err error) bool {
 	NoErrorf is similar to NoError with an extra format string. The first
 	and only variable in the format string should be %v for error.
 */
-func NoErrorf(t *testing.T, fmt string, err error) bool {
+func NoErrorf(t *testing.T, fmtStr string, err error) bool {
 	if err !=  nil {
-		t.Errorf(fmt, err)
+		t.Errorf("%s%s", assertPos(0), fmt.Sprintf(fmtStr, err))
 		return false
 	}
 	return true
